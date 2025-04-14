6f75fc822be6e592cbf10d027d3e23a1b8022e93
Kanirpu <Kapoorni482@gmail.com>
2025-04-14 16:13:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9Ahl2BBa#VYE5qtt8CiaihjpxASKYFoQ3Y_YOxZ6t_5m49lZwCDI",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Wr5nHYRB#YhUoWssp5RXe7gWUoifaURJtwpLStRsnRsBK_iiCizU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tMKrxrqf/bd712813ce24449a.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔-𝗠𝗗 𝙖𝙡𝙞𝙫𝙚 𝙣𝙤𝙬 👋\n\n> *㋛ 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰  〽️Ｄ*"
 };
